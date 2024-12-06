d2addd28235dd833af9eaa448661eb4c36464ba9
RafaelElebiyo <rafaelelebiyomedina1@gmail.com>
2024-12-06 21:37:30 +0100

    second commit

--- Jeu.js
+++ Jeu.js
@@ -1,30 +1,26 @@
-let i = 0
+let i = 0; 
 
 function ajouterBoule() {
+    let boule = document.createElement('div');
+    boule.setAttribute('id', 'boule' + i);
+    boule.className = 'boule';
 
-    let boule = document.createElement('div')
-    boule.setAttribute('id', 'boule' + i)
-    boule.className='boule'
+    let bouleWidth = 100;
+    let ecranWidth = window.innerWidth;
+    let leftPosition = Math.floor(Math.random() * (ecranWidth - (10 + bouleWidth)));
+    boule.style.left = leftPosition + 'px';
 
+    let area = document.getElementById('area');
+    area.appendChild(boule);
 
-    let bouleWidth = 100
-    let ecranWidth = window.innerWidth
-    let leftPosition = Math.floor(Math.random() * (ecranWidth - (10 + bouleWidth))) 
-    boule.style.left = leftPosition + 'px'
-
-
-    let area = document.getElementById('area')
-    area.appendChild(boule)
-
-
-    let topPosition = 0
+    let topPosition = 0;
     let intervalId = setInterval(() => {
-        boule.style.top = (topPosition++) + 'px'
+        boule.style.top = (topPosition++) + 'px';
 
-        if (topPosition >= window.innerHeight -(1+bouleWidth)) {
-            clearInterval(intervalId)  
+        if (topPosition >= window.innerHeight - (1 + bouleWidth)) {
+            clearInterval(intervalId);
         }
-    }, 100)
+    }, 10); 
 
-    i++ 
+    i++; 
 }
